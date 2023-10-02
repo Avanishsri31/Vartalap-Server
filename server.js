86343f62fad49d216c5562a11feddac6e66a28e7
Avanishsri31 <90971881+Avanishsri31@users.noreply.github.com>
2023-10-02 23:32:16 +0530

    Update

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-app.get("/", (req, res) => {
-  res.send("API Running!");
-});
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
@@ -24,19 +24,19 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // --------------------------deployment------------------------------
 
@@ -54,7 +54,7 @@ const server = app.listen(
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://vartalap31.netlify.app/",
+    origin: "http://localhost:3000",
     // credentials: true,
   },
 });
