a310009b9b9104ec0ed6eb43db8fecaaab817579
Avanishsri31 <90971881+Avanishsri31@users.noreply.github.com>
2023-10-03 23:28:29 +0530

    Update

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express"
-import { connectDB }  from "./config/db.js";
+// import { connectDB }  from "./config/db.js";
+import mongoose from "mongoose";
+import colors from "colors";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes  from "./routes/userRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
@@ -7,11 +10,13 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import path from "path";
 import {Server} from "socket.io";
-import { createServer } from "tls";
+import { createServer } from "http";
 
 dotenv.config();
-connectDB();
+// connectDB();
 const app = express();
+const httpServer = createServer(app);
+app.use(cors());
 
 app.use(express.json()); // to accept json data
 
@@ -45,14 +50,25 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-const httpServer = createServer(app);
-httpServer.listen(PORT, (req, res) => {
-  console.log(`Server running one port: ${PORT}`)
-});
+httpServer.listen(
+  PORT, (req, res) =>{
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+  });
 
-const io = new Server(httpServer, {cors: "https://vartalap31.netlify.app/"});
+const DATABASE_URL = process.env.MONGO_URI;
+mongoose.connect(DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true})
+   .then(() => console.log(`MongoDB Connected`.cyan.underline))
+   .catch((err) => console.log(`Error: ${err.message}`.red.bold));
+
+   const io = new Server(httpServer, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "http://localhost:3000",
+      // credentials: true,
+    },
+  });
 
 io.on("connection", (socket) => {
   console.log("Connected to socket.io");
